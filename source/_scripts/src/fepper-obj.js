2820cfa2bce25582d3bc0767fd40e050e35c0c54
e2tha-e <ee@electric-eloquence.io>
2017-01-12 19:07:36 -0500

    fepper-obj.js with defaults

--- source/_scripts/src/fepper-obj.js
+++ source/_scripts/src/fepper-obj.js
@@ -14,20 +14,20 @@
   var FEPPER = FEPPER || {};
   FEPPER.breakpoints = {
     lg: {
-      maxWidth: window.bp_lg_max,
-      minWidth: window.bp_lg_min
+      maxWidth: window.bp_lg_max || -1,
+      minWidth: window.bp_lg_min || 1024
     },
     md: {
-      maxWidth: window.bp_lg_min - 1,
-      minWidth: window.bp_md_min
+      maxWidth: window.bp_lg_min ? window.bp_lg_min - 1 : 1023,
+      minWidth: window.bp_md_min || 768
     },
     sm: {
-      maxWidth: window.bp_md_min - 1,
-      minWidth: window.bp_sm_min
+      maxWidth: window.bp_md_min ? window.bp_md_min - 1 : 767,
+      minWidth: window.bp_sm_min || 480
     },
     xs: {
-      maxWidth: window.bp_sm_min - 1,
-      minWidth: window.bp_xs_min
+      maxWidth: window.bp_sm_min ? window.bp_sm_min - 1 : 479,
+      minWidth: window.bp_xs_min || 0
     }
   };
   window.FEPPER = FEPPER;
