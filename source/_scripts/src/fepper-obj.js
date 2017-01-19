cff20a541b30ca5edb4f6f9c1a91f9784fa4bcb3
e2tha-e <ee@electric-eloquence.io>
2017-01-19 07:10:42 -0500

    updated to use bower for ui

--- source/_scripts/src/fepper-obj.js
+++ source/_scripts/src/fepper-obj.js
@@ -15,18 +15,18 @@
   FEPPER.breakpoints = {
     lg: {
       maxWidth: window.bp_lg_max || -1,
-      minWidth: window.bp_lg_min || 1024
+      minWidth: window.bp_md_max ? window.bp_md_max + 1 : 1025
     },
     md: {
-      maxWidth: window.bp_lg_min ? window.bp_lg_min - 1 : 1023,
-      minWidth: window.bp_md_min || 768
+      maxWidth: window.bp_md_max || 1024,
+      minWidth: window.bp_sm_max ? window.bp_sm_max + 1 : 769
     },
     sm: {
-      maxWidth: window.bp_md_min ? window.bp_md_min - 1 : 767,
-      minWidth: window.bp_sm_min || 480
+      maxWidth: window.bp_sm_max || 768,
+      minWidth: window.bp_xs_max ? window.bp_xs_max + 1 : 481
     },
     xs: {
-      maxWidth: window.bp_sm_min ? window.bp_sm_min - 1 : 479,
+      maxWidth: window.bp_xs_max || 480,
       minWidth: window.bp_xs_min || 0
     }
   };
