c269b55702ba62698edf0419384a0781b1bf79e3
e2tha-e <ee@electric-eloquence.io>
2017-01-19 07:10:42 -0500

    updated to use bower for ui

---
+++ source/_scripts/ui-extender.js
@@ -0,0 +1,5 @@
+/**
+ * Use this file to extend and customize the Fepper UI.
+ * @todo Componentize the UI so the components can be modified if necessary. Allow for pre and postprocessing of these
+ * components.
+ */
