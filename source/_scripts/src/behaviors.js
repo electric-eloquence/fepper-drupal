f77b587d79d1059f35d6c2cac65a431e6d747508
e2tha-e <ee@electric-eloquence.io>
2018-08-10 15:48:24 -0400

    improved mobile navigation

--- source/_scripts/src/behaviors.js
+++ source/_scripts/src/behaviors.js
@@ -6,29 +6,6 @@
 (function ($) {
   'use strict';
 
-  // Check for a footer menu block placed before the branding block, since this
-  // is the default layout on a fresh Drupal 8 standard install (v8.5.5). Such a
-  // layout requires that the mobile search and main menu widgets are pushed an
-  // additional distance from the top of the page.
-  var $branding = $('[id^="block-"][id$="-branding"]');
-  var $brandingPrevSibling;
-  var brandingPrevSiblingId;
-  var hasFooterMenuInHeader;
-
-  if ($branding.length) {
-    $brandingPrevSibling = $branding.prev();
-  }
-
-  if ($brandingPrevSibling.length) {
-    brandingPrevSiblingId = $brandingPrevSibling.attr('id');
-  }
-
-  if (brandingPrevSiblingId) {
-    hasFooterMenuInHeader =
-      brandingPrevSiblingId.slice(0, 6) === 'block-' && brandingPrevSiblingId.slice(-7) === '-footer';
-  }
-
-  // Back to our regularly scheduled program.
   function mobileNavToggle($togglerParent) {
     var $toggler = $togglerParent.find('> h2 > a');
 
@@ -48,20 +25,33 @@
       var $searchBlock = $('div[id^="block-"][id$="-search"]', context);
       var $mainMenuBlock = $('nav[id^="block-"][id$="-main-menu"]', context);
 
-      // Add an additional class if the footer menu block is placed before the
-      // branding block.
-      if (hasFooterMenuInHeader) {
-        if (!$searchBlock.hasClass('wide')) {
-          $searchBlock.addClass('wide');
-        }
+      if ($searchBlock.length) {
+        mobileNavToggle($searchBlock, context);
 
-        if (!$mainMenuBlock.hasClass('wide')) {
-          $mainMenuBlock.addClass('wide');
-        }
+        var $searchToggler = $searchBlock.find('> h2 > a');
+
+        $searchToggler.click(function () {
+          var searchBlockRect = $searchBlock[0].getBoundingClientRect();
+
+          if ($searchBlock.hasClass('open')) {
+            $('#search-block-form', context).css('top', (searchBlockRect.top + searchBlockRect.height) + 'px');
+          }
+        });
       }
 
-      mobileNavToggle($searchBlock, context);
-      mobileNavToggle($mainMenuBlock, context);
+      if ($mainMenuBlock.length) {
+        mobileNavToggle($mainMenuBlock, context);
+
+        var $mainMenuToggler = $mainMenuBlock.find('> h2 > a');
+
+        $mainMenuToggler.click(function () {
+          var mainMenuBlockRect = $mainMenuBlock[0].getBoundingClientRect();
+
+          if ($mainMenuBlock.hasClass('open')) {
+            $mainMenuBlock.children('ul').css('top', (mainMenuBlockRect.top + mainMenuBlockRect.height) + 'px');
+          }
+        });
+      }
     }
   };
 })(jQuery);
