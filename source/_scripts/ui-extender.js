1071e22b89714feea18058422361e06c1772f1b5
e2tha-e <ee@electric-eloquence.io>
2020-05-17 23:21:33 -0400

    style updates

--- source/_scripts/ui-extender.js
+++ source/_scripts/ui-extender.js
@@ -1,5 +1,3 @@
 /**
- * Use this file to extend and customize the Fepper UI.
- * @todo Componentize the UI so the components can be modified if necessary. Allow for pre and postprocessing of these
- * components.
+ * Use this file to extend and customize the client-side scripting of the Fepper UI.
  */
