fbff6f4a0be5f0581b03f5d7cd3ea24676367ba8
e2tha-e <ee@electric-eloquence.io>
2020-09-23 15:10:09 -0400

    updated drupal backend

--- backend/drupal/vendor/autoload.php
+++ backend/drupal/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit7376e166806aa5d664ccbd35d60d770b::getLoader();
+return ComposerAutoloaderInit7320d926879abcbedcc51a3c63fd2d6a::getLoader();
